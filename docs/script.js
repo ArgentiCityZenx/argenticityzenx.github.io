5d0e5e93257fdff134d343a26ad4322975ff7c21
ArgentiCityZenx <164490587+ArgentiCityZenx@users.noreply.github.com>
2024-09-02 15:22:24 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,18 @@
+function toggleMenu() {
+    const navMenu = document.getElementById('nav-menu');
+    if (navMenu.style.display === 'flex' || navMenu.style.display === '') {
+        navMenu.style.display = 'none';
+    } else {
+        navMenu.style.display = 'flex';
+    }
+}
+
 const a ="https://argenticityzenx.github.io/pixel-papercraft-generator-builder/#minecraft-mickey-ears"
-const b = "footer"
-const c = "notification"
-const d = "profile-card"
-const e = "shopping-cart"
-const f = "dashboard"
+const b = "footer/index.html"
+const c = "notification/index.html"
+const d = "profile-card/index.html"
+const e = "shopping-cart/index.html"
+const f = "dashboard/index.html"
 
 function redirect(n) {
     window.location.href = n;
